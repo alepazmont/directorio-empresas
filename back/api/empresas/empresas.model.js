c37088e6b0a584ea5069f4b53f607b27060c4967
Alejandro Paz <alepazmont@gmail.com>
2024-06-04 19:07:39 +0200

    subida cambios back

--- back/api/empresas/empresas.model.js
+++ back/api/empresas/empresas.model.js
@@ -16,7 +16,7 @@ const empresasSchema = new mongoose.Schema({
     email: { type: String, required: true, trim: true },
     web: { type: String }, 
     redes: { type: Array }, 
-    condiciones: { type: Boolean, default: false }
+    condiciones: { type: Boolean, required: true, default: false }
 });
 
 const Empresas = mongoose.model("Empresas", empresasSchema);
