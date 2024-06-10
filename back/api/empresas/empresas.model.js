7b55e5ac5b986397d0ad70980b18eb2845566c56
alepazmont <alepazmont@gmail.com>
2024-06-10 12:48:36 +0200

    actualizacion modelo empresas para redes como objetos

--- back/api/empresas/empresas.model.js
+++ back/api/empresas/empresas.model.js
@@ -7,15 +7,21 @@ const empresasSchema = new mongoose.Schema({
     listaProd: { type: Array },
     listaServ: { type: Array },
     logo: { type: String, required: true, trim: true },
-    galeriaFotos: { type: Array },
+    galeriaFotos: { type: [String] },
     direccion: { type: String, required: true, trim: true },
     codigoPostal: { type: Number, required: true, trim: true },
     paradaMetro: { type: String },
-    locMapa: { type: Array, required: true },
-    telefono: { type: Array, trim: true },
+    locMapa: { type: [Number], required: true },
+    telefono: { type: [String], trim: true },
     email: { type: String, required: true, trim: true },
     web: { type: String },
-    redes: [{ redSocial: String, url: String }],
+    redes: [{
+        redSocial: {
+            type: String,
+            enum: ['Facebook', 'Twitter', 'Instagram', 'LinkedIn', 'YouTube', 'Otra']
+        },
+        url: String
+    }],
     condiciones: { type: Boolean },
     aprobada: { type: Boolean, required: true, default: false },
     popularidad: { type: Number, default: 0, min: [0, 'La popularidad debe ser un número positivo'] }
